72486d7f1805a004714a48fefa95b146120d7919
John Jacobsen <john@mail.npxdesigns.com>
2014-12-07 20:10:39 -0600

    Clean up project.clj.

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject nbodybox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "WebGL and Node Webkit conquer the world"
+  :url "https://github.com/eigenhombre/n-body-box"
+  :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
-                 [enfocus "2.1.1"]
-                 [jayq "2.5.2"]]
+                 [enfocus "2.1.1"]]
   :source-paths ["src/clj"]
   :main ^:skip-aot nbodybox.core
   :target-path "target/%s"
@@ -15,7 +13,7 @@
   :cljsbuild {:builds [{:source-paths ["src/cljs"]
                         :compiler {:output-to "resources/public/client.js"
                                    :optimizations :whitespace
-                                   :pretty-print true } }] }
+                                   :pretty-print true}}]}
   :node-webkit-build {:root "resources/public"
                       :osx { :icon "resources/icons/myicon.icns"}
                       :name "NBodyBox"}
